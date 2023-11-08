f4fbca4bfd9a0ace3efb29576b6dee2c9ad9920d
Yagiar <alexrumling2000@gmail.com>
2023-11-08 23:32:53 +0300

    add login and registration

--- MyDataBaseApp/Form1.Designer.cs
+++ MyDataBaseApp/Form1.Designer.cs
@@ -28,13 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.auth = new System.Windows.Forms.Button();
+            this.username = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.registration = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // auth
+            // 
+            this.auth.Location = new System.Drawing.Point(355, 174);
+            this.auth.Name = "auth";
+            this.auth.Size = new System.Drawing.Size(108, 23);
+            this.auth.TabIndex = 0;
+            this.auth.Text = "Войти";
+            this.auth.UseVisualStyleBackColor = true;
+            this.auth.Click += new System.EventHandler(this.auth_Click);
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(234, 97);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(338, 22);
+            this.username.TabIndex = 1;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(234, 146);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(338, 22);
+            this.password.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(341, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Имя пользователя";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(374, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Пароль";
+            // 
+            // registration
+            // 
+            this.registration.Location = new System.Drawing.Point(355, 203);
+            this.registration.Name = "registration";
+            this.registration.Size = new System.Drawing.Size(108, 23);
+            this.registration.TabIndex = 5;
+            this.registration.Text = "Регистрация";
+            this.registration.UseVisualStyleBackColor = true;
+            this.registration.Click += new System.EventHandler(this.registration_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.registration);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.auth);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button auth;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button registration;
     }
 }
 
--- MyDataBaseApp/Form1.cs
+++ MyDataBaseApp/Form1.cs
@@ -6,15 +6,129 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 using System.Windows.Forms;
+using Npgsql;
 
 namespace MyDataBaseApp
 {
     public partial class Form1 : Form
     {
+        private Database db;
+
         public Form1()
         {
             InitializeComponent();
+            db = new Database("Host=pgdb.uni-dubna.ru;Username=student25;Password=Io_228_1337;Database=student25");
+        }
+
+        private void auth_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (db.OpenConnection())
+                {
+                    string request = "SELECT * FROM users WHERE username = @username";
+                    NpgsqlCommand npgSqlCommand = new NpgsqlCommand(request, db.Connection);
+                    npgSqlCommand.Parameters.AddWithValue("@username", username.Text);
+
+                    using (NpgsqlDataReader npgSqlDataReader = npgSqlCommand.ExecuteReader())
+                    {
+                        if (npgSqlDataReader.HasRows)
+                        {
+                            MessageBox.Show("Вход успешен");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Фейл с входом");
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Нет соединения");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
+            finally
+            {
+                db.CloseConnection();
+            }
+        }
+
+        private void registration_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (db.OpenConnection())
+                {
+                    string select_request = "SELECT * FROM users WHERE username = @username";
+                    NpgsqlCommand select_npgSqlCommand = new NpgsqlCommand(select_request, db.Connection);
+                    select_npgSqlCommand.Parameters.AddWithValue("@username", username.Text);
+
+                    bool userExists = false;
+
+                    using (NpgsqlDataReader select_npgSqlDataReader = select_npgSqlCommand.ExecuteReader())
+                    {
+                        userExists = select_npgSqlDataReader.HasRows;
+                    }
+
+                    if (!userExists)
+                    {
+                        string insert_request = "INSERT INTO users (username, hash_pwd) VALUES (@username, @hash_pwd);";
+                        NpgsqlCommand insert_npgSqlCommand = new NpgsqlCommand(insert_request, db.Connection);
+                        insert_npgSqlCommand.Parameters.AddWithValue("@username", username.Text);
+                        insert_npgSqlCommand.Parameters.AddWithValue("@hash_pwd", CalculateMD5Hash(password.Text));
+
+                        int rowsAffected = insert_npgSqlCommand.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Регистрация успешна, можете войти");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Произошла ошибка при регистрации");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Пользователь с данным именем уже существует");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Нет соединения");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
+            finally
+            {
+                db.CloseConnection();
+            }
+        }
+
+        public string CalculateMD5Hash(string input)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("x2")); // Преобразование в шестнадцатеричную строку
+                }
+
+                return sb.ToString();
+            }
         }
     }
+
 }
